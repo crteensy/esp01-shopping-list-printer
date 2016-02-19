c2f339ce4cb9b7655e335106cafe95c1cde9f5c7
crteensy <redecker@avrbeginners.net>
2016-02-19 19:22:24 +0100

    removed ssid/password

--- sketch/settings.cpp
+++ sketch/settings.cpp
@@ -1,8 +1,8 @@
 #include "settings.h"
 #include "localization.h"
 
-const char* ssid = "FRITZ!Box 7490";
-const char* password = "24742474779474603393";
+const char* ssid = "YOUR SSID";
+const char* password = "YOUR PASSWORD";
 const char* devicename = "shoppinglist"; // your device will be discoverable at http://devicename.local
 const int iLanguage = ELanguage::eEnglish;
 const uint16_t serverHttpPort = 80;
